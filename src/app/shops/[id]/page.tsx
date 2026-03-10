49c6e2ca0c8f67c8cd8fcb1a759cc6825c9262e1
Administrator <admin@example.com>
2026-03-10 19:27:56 +0530

    Remove duplicate session declarations

--- src/app/shops/[id]/page.tsx
+++ src/app/shops/[id]/page.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 
 
 export default async function ShopDetails({ params }: { params: { id: string } }) {
-    const session = await auth();
-    const userId = (session?.user as any)?.id;
+  //const session = await auth();
+  //const userId = (session?.user as any)?.id;
   // Await params in Next.js 15+
   const resolvedParams = await params;
   const session = await auth();
